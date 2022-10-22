408a577e5ad57f3e68e0ecd82a34ca1e40e528de
naseif <d2mds21@gmail.com>
2022-10-22 15:17:54 +0200

    Deleted test.js

--- tests.js
+++
@@ -1,8 +0,0 @@
-const { TTScraper } = require("./dist/main");
-
-const scraper = new TTScraper();
-
-(async () => {
-  const test = await scraper.video("https://vm.tiktok.com/ZMNcstcjU/", true);
-  console.log(test);
-})();
